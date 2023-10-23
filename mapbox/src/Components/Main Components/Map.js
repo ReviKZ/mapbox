2d582a20727542d499de753c4471da40b26f6807
ReviKZ <kovacs.zoard.1@gmail.com>
2023-10-23 11:16:57 +0200

    Add: Create Map Component Add: Set Default Center to Veszprém Style: Import default styling

---
+++ mapbox/src/Components/Main Components/Map.js
@@ -0,0 +1,33 @@
+import React, { useRef, useEffect, useState } from 'react';
+import '../../Styling/Map.css';
+import mapboxgl from 'mapbox-gl';
+
+const Map = () => {
+    mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN //Getting access token from environmental variables.
+
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(17.907929); //Setting base coordinates & Zoom to Veszprém
+    const [lat, setLat] = useState(47.094056);
+    const [zoom, setZoom] = useState(13);
+
+    useEffect(() => {
+        if (map.current) return; //Render map only once
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v12',
+            center: [lng, lat],
+            zoom: zoom
+        });
+    });
+
+    return (
+        <div>
+            {/* Importing default styling like this since "import 'mapbox-gl/dist/mapbox-gl.css';" did not work! */}
+            <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.53.0/mapbox-gl.css' rel='stylesheet' />
+            <div ref={mapContainer} className="map-container" />
+        </div>
+    );
+}
+
+export default Map;
