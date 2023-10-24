c47b4480cac444210dd6312060c3dfc4db06cd84
ReviKZ <kovacs.zoard.1@gmail.com>
2023-10-24 17:11:00 +0200

    Doc: Add some comments

--- mapbox/src/Components/Main Components/Map.js
+++ mapbox/src/Components/Main Components/Map.js
@@ -16,40 +16,40 @@ const Map = () => {
     const [lat, setLat] = useState(47.094056);
     const [zoom, setZoom] = useState(13);
 
-    const nav = new mapboxgl.NavigationControl();
-    const searchBox = new MapboxGeocoder({
+    const nav = new mapboxgl.NavigationControl(); //Add Navigation Control
+    const searchBox = new MapboxGeocoder({ //Add Search Box
         accessToken: mapboxgl.accessToken,
         marker: false,
         mapboxgl: mapboxgl
     });
 
-    const [navList, setNavList] = useState([]);
+    const [navList, setNavList] = useState([]); //A List for markers, to navigate between them
 
-    function removeMarker(index) {
+    function removeMarker(index) { //Remove Marker from the list
         navList[index].marker.remove();
         let currentList = [...navList];
         let modifiedList = currentList.filter(marker => marker !== currentList[index]);
         setNavList(modifiedList);
     }
 
-    const [route, setRoute] = useState({});
-    const [time, setTime] = useState(0);
-    const [distance, setDistance] = useState(0);
+    const [route, setRoute] = useState({}); //Route,
+    const [time, setTime] = useState(0); //Time &
+    const [distance, setDistance] = useState(0); //Distance data for visualizing
 
     async function planRoute() {
-        var markersString = "";
+        var markersString = ""; //Creating the request string as described in the mapbox documentation
         for (var i = 0; i < navList.length; i++) {
             markersString += `${navList[i].marker._lngLat.lng},${navList[i].marker._lngLat.lat};`
         }
-        var result = await PlanRoute(markersString);
+        var result = await PlanRoute(markersString); //Sending the request
         setRoute(result);
     }
 
-    const [color, setColor] = useState("#378c5a");
-    const [width, setWidth] = useState(4);
+    const [color, setColor] = useState("#378c5a"); //Line color setting
+    const [width, setWidth] = useState(4); //Line width setting
 
     function changeLine(event) {
-        event.preventDefault();
+        event.preventDefault(); //On [Save] button click, gets the value and sets it as the variable
         setColor(document.getElementById("color").value);
         setWidth(parseInt(document.getElementById("width").value));
     }
@@ -62,11 +62,11 @@ const Map = () => {
             center: [lng, lat],
             zoom: zoom
         });
-        map.current.addControl(searchBox);
+        map.current.addControl(searchBox); //Adding the searchbox & Navigation to the layer
         map.current.addControl(nav);
 
         map.current.on('click', (e) => {
-            const marker = new mapboxgl.Marker().setLngLat(e.lngLat).addTo(map.current);
+            const marker = new mapboxgl.Marker().setLngLat(e.lngLat).addTo(map.current); //Adding marker to the layer and to the navList
             setNavList((prevNavList) => [
                 ...prevNavList,
                 {marker},
@@ -74,7 +74,7 @@ const Map = () => {
         });
 
         searchBox.on('result', (result) => {
-            const marker = new mapboxgl.Marker().setLngLat(result.result.center).addTo(map.current);
+            const marker = new mapboxgl.Marker().setLngLat(result.result.center).addTo(map.current); //Adding marker from the searchbox result coordinates to the layer and to navList 
             setNavList((prevNavList) => [
                 ...prevNavList,
                 {marker},
@@ -82,7 +82,7 @@ const Map = () => {
         });
 
         map.current.on('load', () => {
-            map.current.addLayer({
+            map.current.addLayer({ //Adding navigation Layer with base data
                 id: 'route',
                 type: 'line',
                 source: {
@@ -105,12 +105,12 @@ const Map = () => {
     useEffect(() => {
         if (map.current.getSource('route')) {
             if (route) {
-                map.current.getSource('route').setData(route.route);
+                map.current.getSource('route').setData(route.route); //Set route data from query
                 setDistance((route.distance / 1000).toFixed(1));
                 setTime(Math.floor(route.time / 60));
             }
             else if (!route) {
-                map.current.getSource('route').setData(
+                map.current.getSource('route').setData( //Reset the data if query was faulty (eg.: One or less marker)
                     {
                         type: 'Feature',
                         properties: {},
@@ -127,7 +127,7 @@ const Map = () => {
 
     useEffect(() => {
         if (map.current.getSource('route')) {
-            map.current.setPaintProperty('route', 'line-color', color);
+            map.current.setPaintProperty('route', 'line-color', color); //Set paint property on route layer's line if there is a change in color or width
             map.current.setPaintProperty('route', 'line-width', width);
         }
     }, [color, width])
