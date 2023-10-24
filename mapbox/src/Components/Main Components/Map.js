b233becb2b846117250d3b7aa3cd27dfb1c799d5
ReviKZ <kovacs.zoard.1@gmail.com>
2023-10-24 12:43:08 +0200

    Add: Import PlanRoute function for route, duration, distance display. Add: Create query string creator for request Add: Add layer on map for route displaying Add: Create other useEffect Loop, which refreshes route display

--- mapbox/src/Components/Main Components/Map.js
+++ mapbox/src/Components/Main Components/Map.js
@@ -5,6 +5,7 @@ import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import Nav from './Nav';
 import mapboxgl from 'mapbox-gl';
+import PlanRoute from '../Functions/PlanRoute';
 
 const Map = () => {
     mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN //Getting access token from environmental variables.
@@ -29,6 +30,19 @@ const Map = () => {
         let currentList = [...navList];
         let modifiedList = currentList.filter(marker => marker !== currentList[index]);
         setNavList(modifiedList);
+    }
+
+    const [route, setRoute] = useState({});
+    const [time, setTime] = useState(0);
+    const [distance, setDistance] = useState(0);
+
+    async function planRoute() {
+        var markersString = "";
+        for (var i = 0; i < navList.length; i++) {
+            markersString += `${navList[i].marker._lngLat.lng},${navList[i].marker._lngLat.lat};`
+        }
+        var result = await PlanRoute(markersString);
+        setRoute(result);
     }
 
     useEffect(() => {
@@ -57,12 +71,57 @@ const Map = () => {
                 {marker},
             ]);
         });
+
+        map.current.on('load', () => {
+            map.current.addLayer({
+                id: 'route',
+                type: 'line',
+                source: {
+                    type: 'geojson',
+                    data: route,
+                },
+                layout: {
+                    'line-join': 'round',
+                    'line-cap': 'round',
+                },
+                paint: {
+                    'line-color': '#378c5a',
+                    'line-width': 4,
+                    'line-opacity': 0.8,
+                },
+            });
+        })
     });
+
+    useEffect(() => {
+        if (map.current.getSource('route')) {
+            if (route) {
+                map.current.getSource('route').setData(route.route);
+                setDistance((route.distance / 1000).toFixed(1));
+                setTime(Math.floor(route.time / 60));
+            }
+            else if (!route) {
+                map.current.getSource('route').setData(
+                    {
+                        type: 'Feature',
+                        properties: {},
+                        geometry: {
+                            type: 'LineString',
+                            coordinates: 0
+                        }
+                    });
+                setTime(0);
+                setDistance(0);
+            }
+        }
+    }, [route]);
+
+    
 
     return (
         <div>
             <div ref={mapContainer} className="map-container" />
-            <Nav coordList={navList} removeMarker={removeMarker}  />
+            <Nav coordList={navList} removeMarker={removeMarker} planRoute={planRoute} distance={distance} time={time}  />
         </div>
     );
 }
